428c47f698712ec539edc370497d86ffe8b4ead4
tim10221 <ejrhgk16@naver.com>
2026-02-14 03:30:20 +0900

    ci/cd 정상테스트

--- blueocean_auto_test/tests/api/LandingApi.js
+++ blueocean_auto_test/tests/api/LandingApi.js
@@ -19,7 +19,7 @@ class LandingApi {
    * @param {string} pageCode
    */
   async getContentList(storeCode, pageCode) {
-    return this.request.get(`${BASE_URL}/landing/content/list233`, {
+    return this.request.get(`${BASE_URL}/landing/content/list`, {
       params: { store_code: storeCode, page_code: pageCode },
     });
   }
